c5ece47852c9bd8ea54589e9654423f41be2cb0b
Greg Howes <gihowes@gmail.com>
2023-05-15 21:10:14 +0900

    Functionality complete All basic functions are working - - draw grid of variable size - validations for user input - box change color on mouseover by listening and adding class
    It needs to be made prettier though!

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 const gridContainer = document.querySelector(".container");
 
 drawGrid(16);
+pointerOn();
+pointerOff();
+//const box = document.querySelectorAll(".box");
+//box.classList.add("active");
+
+function pointerOn() {
+    document.querySelectorAll('.box').forEach(box => {
+        box.addEventListener('mouseover', event => {
+        //handle click
+        //console.log("Hello");
+        box.classList.add("active");
+        })
+  })
+}
+
+function pointerOff() {
+    document.querySelectorAll('.box').forEach(box => {
+    box.addEventListener('mouseout', event => {
+        //handle click
+        box.classList.remove("active");
+        })
+    })
+}
+
+
+const sizeButton = document.querySelector("#btn-change-size");
+sizeButton.addEventListener("click", event => {
+    const gridSize = prompt("Please select the number of boxes on each side of the grid, from 8 to 100");
+    if (gridSize < 8 || gridSize > 100) {
+        alert("Incorrect grid size!");
+    } else {
+        removeAllChildNodes(gridContainer);
+        drawGrid(gridSize);
+    }
+})
 
 function drawGrid(squarePerSide) {
     
@@ -11,9 +46,17 @@ function drawGrid(squarePerSide) {
         for (column = 0; column < squarePerSide; column++) {
             const newBox = document.createElement("div");
             newBox.classList.add("box");
-            newBox.innerText = column;
             newRow.appendChild(newBox);
         }
         gridContainer.appendChild(newRow);
+    }
+    pointerOn();
+    pointerOff();
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        //wait(100);
+        parent.removeChild(parent.firstChild);
     }
 }
